f18df49a38384c057c896558b5a4c16c4037b8ba
aaoio <aaoio>
2022-11-10 15:12:16 +0800

    - Match bind mount target paths of airflow containers and spark containers - Add geopandas to requirements.txt for Dockerfiles for airflow and spark images - Change paths in dags/scripts/paths.py to reflect change in docker-compose.yaml

--- airflow/dags/scripts/paths.py
+++ airflow/dags/scripts/paths.py
@@ -1,8 +1,7 @@
 import os
 
-ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
 WEBDRIVER_PATH = '/usr/local/bin/geckodriver'
-DATA_DIR = os.path.join(ROOT_DIR, 'data')
+DATA_DIR = '/usr/local/data/raw'
 
 export_XPath = '//button[@data-ide-handler="click:onExportClick"]'
 geojson_XPath = '//p[@id="export-geoJSON"]/div[@class="field-body"]/span/a[@title="saves the exported data as a file"]'
